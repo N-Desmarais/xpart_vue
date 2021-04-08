aacbfd9ec8039f1398bda6d9103ffae9e3074b38
N-Desmarais <55117352+N-Desmarais@users.noreply.github.com>
2021-04-08 05:25:36 -0400

    fixed axios base url

--- src/main.js
+++ src/main.js
@@ -14,13 +14,13 @@ import { Auth0Plugin } from "./auth";
 Vue.use(Auth0Plugin, {
   domain,
   clientId,
-  onRedirectCallback: appState => {
-    router.push(
-      appState && appState.targetUrl
-        ? appState.targetUrl
-        : window.location.pathname
-    );
-  }
+  // onRedirectCallback: appState => {
+  //   router.push(
+  //     appState && appState.targetUrl
+  //       ? appState.targetUrl
+  //       : window.location.pathname
+  //   );
+  //}
 });
 
 new Vue({
